84f597eac5637b3679cd9cb7ecb71f55c8740501
Lyciih <gnehciyil@gmail.com>
2023-03-08 02:07:11 +0800

    printf bug

--- week2/tb/testbench.cpp
+++ week2/tb/testbench.cpp
@@ -63,7 +63,7 @@ int main(int argc,char **argv)
     for(int i = 0; i < 32; i++)
     {
 	    int x = sim_regs_read(top->test_top->regfile0, i);
-	    printf(" x%02d = %08x% -- ", i, x);
+	    printf(" x%02d = %08x ", i, x);
 	    if(i % 4 == 3){printf("\n");}
     }
 
